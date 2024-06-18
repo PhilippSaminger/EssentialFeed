09472cc9174109c7ca0945035378209e2cfee948
Philipp Saminger <Philipp.Saminger@netcetera.com>
2024-06-18 16:40:12 +0200

    Move Remote and LocalFeedItem to own files

---
+++ EssentialFeed/Feed API/RemoteFeedItem.swift
@@ -0,0 +1,10 @@
+//  Created by Philipp Saminger on 18.06.24.
+
+import Foundation
+
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
