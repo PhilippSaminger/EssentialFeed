8104951098c387f0b5247c713f495b9b77e96339
Philipp Saminger <Philipp.Saminger@netcetera.com>
2024-10-22 15:45:36 +0200

    Move FeedViewController to production

---
+++ EssentialFeed/EssentialFeediOS/FeedViewController.swift
@@ -0,0 +1,37 @@
+//  Created by Philipp Saminger.
+
+import UIKit
+import EssentialFeed
+
+public class FeedViewController: UITableViewController {
+    private var loader: FeedLoader?
+    private var viewAppeared = false
+    
+    public convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+    }
+    
+    public override func viewIsAppearing(_ animated: Bool) {
+        super.viewIsAppearing(animated)
+        
+        if !viewAppeared {
+            load()
+            viewAppeared = true
+        }
+    }
+    
+    @objc private func load() {
+        refreshControl?.beginRefreshing()
+        loader?.load { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
+    }
+}
